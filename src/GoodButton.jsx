5e6775e2d5d4022b64dbb84064e6735a4acf31cf
= <neymegen50@gmail.com>
2022-07-29 22:23:25 +0300

    message

--- src/GoodButton.jsx
+++ src/GoodButton.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
 const GoodButton = () => {
-  <button 
+  return(
+    <button 
   className="fancy-button"
   onClick={() => alert('Good job!')}
   >
     Click me!
   </button>
+  );
+  
 };
 
 export default GoodButton;
